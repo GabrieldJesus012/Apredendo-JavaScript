3657bda444754ef63c20921379a684bfe601221c
Gabriel de Jesus <gabrieldjesus055@outlook.com>
2024-02-21 18:08:25 -0300

    att

---
+++ exercicios/ex018/ambiente.js
@@ -0,0 +1,9 @@
+let num = [1,0,8,3,2]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos=num.indexOf(8)
+console.log(`O valor 8 esta na posição ${pos}`)
